8392999dd2684197c24f91a74c8a7c5127061f9f
npr96 <npr96@gmail.com>
2020-02-26 08:48:33 -0700

    commiting homework

---
+++ homework1.py
@@ -0,0 +1,25 @@
+# name = 'Alex'
+# length = len(name)
+# print(length)
+
+# greeting = 'Howdy'
+# name = 'Alex'
+# print(greeting + ' ' + name)
+
+# name = 'Alex'
+# print(name[-1] + name[1:-1] + name[0])
+
+# sum_variable = 0
+# Summation_string = [1,2,3,4]
+# for i in Summation_string:
+#     sum_variable = i + sum_variable
+# print(sum_variable)
+
+# string_longest_word = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']
+# sorted_longest_word = sorted(string_longest_word, key=len)
+# print(sorted_longest_word[-1])
+
+# string_replace = 'racecar driver'
+# replacer_value = string_replace[0]
+# modified_string = string_replace.replace(replacer_value, '$')
+# print(replacer_value+modified_string[1:])
